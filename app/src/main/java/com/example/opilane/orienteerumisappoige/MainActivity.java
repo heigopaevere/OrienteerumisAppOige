46c33657e7b403c4a3f000d1d9564f61a4d6f4b5
heigopaevere <heigopaevere@gmail.com>
2018-05-15 21:04:58 +0300

    _ _ _ _ _  _

--- app/src/main/java/com/example/opilane/orienteerumisappoige/MainActivity.java
+++ app/src/main/java/com/example/opilane/orienteerumisappoige/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.opilane.orienteerumisappoige;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -22,5 +24,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         legend = findViewById(R.id.kaardilegend);
+        abi = findViewById(R.id.abi);
+        kompass = findViewById(R.id.kompass);
+        signs = findViewById(R.id.margid);
+
+        abi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, abiActivity.class));
+
+            }
+        });
+        kompass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+            }
+        });
     }
 }
